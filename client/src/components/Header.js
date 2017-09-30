e0bd3ecb08244f130e293315454ce38cf6ddb5e6
timchen <timmy.i.chen@gmail.com>
2017-09-29 21:50:39 -0400

    heroku deployment

---
+++ client/src/components/Header.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import Payments from './Payments';
+
+class Header extends Component {
+	renderContent() {
+		const { auth } = this.props;
+		switch(auth) {
+			case null:
+				return;
+			case false:
+			  return (
+						<li>
+							<a href="/auth/google">Login with Google</a>
+						</li>
+			  	);
+			default:
+				return ([
+						<li key="0">Credits: {auth.credits}</li>,
+						<li key="1"><Payments /></li>,
+						<li key="2"><a href="/api/logout">Logout</a></li>
+					]);
+		}
+	}
+	render() {
+		const { auth } = this.props;
+		return (
+			<nav>
+				<div className="nav-wrapper">
+					<Link 
+						to={auth ? '/surveys' : '/'}
+						className="left brand-logo"
+					>
+						Emaily
+					</Link>
+					<ul className="right">
+						{this.renderContent()}
+					</ul>
+				</div>
+			</nav>
+		)
+	}
+}
+
+function mapStateToProps({ auth }) {
+	return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
